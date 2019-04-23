6e2abb1b3c52451b3e2a8ae011116f25291fff1a
Eason Kang <eason.kang@autodesk.com>
2019-04-23 18:49:23 +0800

    Basic Viewer

--- www/js/index.js
+++ www/js/index.js
@@ -22,5 +22,69 @@
 
   // Put your code here
 
-  
+  function fetchForgeToken(callback) {
+    fetch('https://forge-token-srv.herokuapp.com/api/forge/oauth/token', {
+      method: 'get',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+        return Promise.reject(
+          new Error(`Failed to fetch token from server (status: ${response.status}, message: ${response.statusText})`),
+        );
+      })
+      .then((data) => {
+        if (!data) return Promise.reject(new Error('Empty token response'));
+
+        callback(data.access_token, data.expires_in);
+      })
+      .catch(error => console.error(error));
+  }
+
+  function onLoadModelSuccess(model) {
+    console.log('onLoadModelSuccess()!');
+    console.log(model);
+  }
+
+  function onLoadModelError(viewerErrorCode) {
+    console.error(`onLoadModelError() - errorCode:${viewerErrorCode}`);
+  }
+
+  function onDocumentLoadSuccess(doc) {
+    const rootItem = doc.getRoot();
+    const filter = { type: 'geometry', role: '3d' };
+    const viewables = rootItem.search(filter);
+
+    if (viewables.length === 0) {
+      console.error('Document contains no viewables.');
+      return;
+    }
+
+    // 初始化 Viewer 物件
+    const viewerDiv = document.getElementById('viewer');
+    const viewer = new Autodesk.Viewing.Private.GuiViewer3D(viewerDiv);
+
+    // 載入 Viewer 物件
+    const initialViewable = viewables[0];
+    viewer.startWithDocumentNode(doc, initialViewable)
+      .then(onLoadModelSuccess)
+      .catch(onLoadModelError);
+  }
+
+  function onDocumentLoadFailure(viewerErrorCode) {
+    console.error(`onDocumentLoadFailure() - errorCode:${viewerErrorCode}`);
+  }
+
+  const documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bGt3ZWo3eHBiZ3A2M3g0aGwzMzV5Nm0yNm9ha2dnb2YyMDE3MDUyOHQwMjQ3MzIzODZ6L2Fkc2stZm9yZ2UtaGVsbG93b3JsZC5ydnQ';
+
+  const initializerOptions = {
+    env: 'AutodeskProduction',
+    getAccessToken: fetchForgeToken,
+  };
+
+  Autodesk.Viewing.Initializer(initializerOptions, () => {
+    Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+  });
 })();
